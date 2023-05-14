4106450b3da1f3148f1344d071815338859d4451
Rahil Jain <f20170541@pilani.bits-pilani.ac.in>
2023-05-14 23:41:13 +0530

    fixed refresh connectivity graph

--- connectivityGraph.py
+++ connectivityGraph.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from constants import *
+import random
 
 class ConnectivityGraph:
     def create_connectivity_graph(app):
@@ -14,32 +15,34 @@ class ConnectivityGraph:
         app.f = plt.Figure(figsize=(3, 3), dpi=100)
         app.ax = app.f.add_subplot(111)
         app.graph.clear()
-        app.graph.add_edges_from(app.game_data.adjacency_list, color='grey')
-        print(app.game_data.current_box_adjacency)
-        attrs = {(i, j): {"color": 'green'} for i,j in app.game_data.current_box_adjacency}
-        nx.set_edge_attributes(app.graph, attrs)
+        app.graph.add_edges_from(app.game_data.adjacency_list, color="red")
         colors = [app.graph[u][v]['color'] for u,v in app.graph.edges()]
-        # nx.draw(g, pos=nx.spectral_layout(g), ax=ax, with_labels=True, node_color="#ADD8E6")
-        nx.draw(app.graph, pos=nx.spring_layout(app.graph), ax=app.ax, with_labels=True,
+        nx.draw(app.graph, pos=nx.spring_layout(app.graph, seed=1), ax=app.ax, with_labels=True,
                  node_color="#ADD8E6", edge_color=colors)
-        # nx.draw_planar(g, ax=ax, with_labels=True, node_color="#ADD8E6")
         canvas = FigureCanvasTkAgg(app.f, app.connectivity_graph_frame)
 
         app.connectivity_graph_canvas = canvas.get_tk_widget()
         app.connectivity_graph_canvas.grid(row=0,column=0)
+
+        refresh_btn = tk.Button(app.connectivity_graph_frame, text="Rearrange Graph", command=lambda: ConnectivityGraph.refresh_connectivity_graph(app,random.randint(2,100) ))
+        refresh_btn.grid(row=1,column=0)
         print(app.connectivity_graph_canvas, "canv")
 
-    def refresh_connectivity_graph(app):
+    def refresh_connectivity_graph(app, seed=1):
         print("refresh called")
         plt.clf()
         app.connectivity_graph_canvas.destroy()
         app.ax.remove()
         app.ax = app.f.add_subplot(111)
+
+        attrs = {(i, j): {"color": 'red'} for i,j in app.game_data.adjacency_list}
+        nx.set_edge_attributes(app.graph, attrs)
         attrs = {(i, j): {"color": 'green'} for i,j in app.game_data.current_box_adjacency}
+        print(app.game_data.current_box_adjacency)
         nx.set_edge_attributes(app.graph, attrs)
 
         colors = [app.graph[u][v]['color'] for u,v in app.graph.edges()]
-        nx.draw(app.graph, pos=nx.spring_layout(app.graph), ax=app.ax, with_labels=True,
+        nx.draw(app.graph, pos=nx.spring_layout(app.graph, seed=seed), ax=app.ax, with_labels=True,
                  node_color="#ADD8E6", edge_color=colors, width=2)
 
         canvas = FigureCanvasTkAgg(app.f, app.connectivity_graph_frame)
