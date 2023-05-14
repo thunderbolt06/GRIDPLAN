9e09bbb237cbb4981b53779f336356c637a293a7
Rahil Jain <f20170541@pilani.bits-pilani.ac.in>
2023-05-14 15:28:30 +0530

    ui complete

--- gui.py
+++ gui.py
@@ -7,8 +7,8 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-helv15 = ("Helvetica", 15, "bold")
-helv8 = ("Helvetica", 8, "bold")
+helv15 = ("Helvetica", 30, "bold")
+helv8 = ("Helvetica", 20, "bold")
 colors = [
     "#7B68EE",  # medium slate blue
     "#40E0D0",  # turqouise
@@ -58,16 +58,101 @@ hex_colors = [
 
 class GameData:
     def __init__(self) -> None:
-        self.adjacency_graph = [[0,1], [1,2], [2,0]]
-        self.box_sizes = [[1,1], [1,1], [2,1], [1,1], [1,1], [2,1]]
-
+        self.adjacency_list = [[0,1], [1,2], [2,0]]
+        self.box_sizes = [[1,1], [1,1], [2,1]]
+        self.num_boxes = 3
+        self.init_graph = nx.Graph()
+        self.init_graph.add_edges_from(self.adjacency_list)
+        self.width_quanta = 50
+        self.height_quanta = 50
         self.rfp = None
-        self.grid_row_size = 5
-        self.grid_col_size = 4
+        self.grid_row_size = 2
+        self.grid_col_size = 2
+        self.box_frames = [None] * 6
+        self.boxes_init_x = [0] * 6
+        self.boxes_init_y = [0] * 6
+        self.boxes_cur_x = [0] * 6
+        self.boxes_cur_y = [0] * 6
+        self.current_box_graph = nx.Graph()
+        self.grid_coords = [100, 100, self.grid_row_size*50, self.grid_col_size*50]
+        
+
+        self.current_box_adjacency = []
+        self.current_box_graph.add_edges_from(self.current_box_adjacency)
+
+    def check_two_boxes_adjacent(self, x1, y1, w1, h1, x2, y2, w2, h2):
+        if(
+            (
+                # Horizontally adjacent
+                ( x1 + w1 == x2 or x2 + w2 == x1 ) and
+                y2 + h2 > y1 and
+                y2 < y1 + h1
+            )
+            or 
+            (
+                # Vertically adjacent
+                ( y1 + h1 == y2 or y2 + h2 == y1 ) and
+                x2 + w2 > x1 and
+                x2 < x1 + w1
+            )
+        ):
+            return True
+        else:
+            return False
+    def get_current_box_adjacency(self):
+        self.current_box_adjacency = []
+        for i in range(self.num_boxes):
+            for j in range(self.num_boxes):
+                if(i < j):
+                    print("adj for", i, j)
+                    if( self.check_two_boxes_adjacent(self.boxes_cur_x[i], self.boxes_cur_y[i], self.width_quanta*self.box_sizes[i][0],self.height_quanta*self.box_sizes[i][1]
+                                                      , self.boxes_cur_x[j], self.boxes_cur_y[j], self.width_quanta*self.box_sizes[j][0],self.height_quanta*self.box_sizes[j][1] )):
+                        self.current_box_adjacency.append([i,j])
+                        print(i,j, "are adjacent")      
+                    print(self.boxes_cur_x[i], self.boxes_cur_y[i])
+                    print(self.boxes_cur_x[j], self.boxes_cur_y[j])
+                    pass
+        self.current_box_graph.clear()
+        self.current_box_graph.add_edges_from(self.current_box_adjacency)
+    
+    def check_game_success(self, label):
+        if(self.check_if_box_satisfy_adj() and self.check_if_all_boxes_are_inside_grid()):
+
+            label.config(text="Congratulations!!! You have completed the level")
+        else :
+            label.config(text="Incorrect!! please try again")
+
+    def check_one_box_is_inside_grid(self,x, y, w, h):
+        if(x >= self.grid_coords[0] and 
+           y >= self.grid_coords[1] and 
+           x + w <= self.grid_coords[0] + self.grid_coords[2] and 
+           y + h <= self.grid_coords[1] + self.grid_coords[3]):
+            return True
+        return False
+
+    def check_if_all_boxes_are_inside_grid(self):
+        for i in range(self.num_boxes):
+            if(self.check_one_box_is_inside_grid(self.boxes_cur_x[i], self.boxes_cur_y[i], self.width_quanta*self.box_sizes[i][0],self.height_quanta*self.box_sizes[i][1] ) == False):
+                return False
+        return True
+
+    def check_if_box_satisfy_adj(self):
+        self.get_current_box_adjacency()
+        print(self.current_box_graph.edges())
+        print(self.init_graph.edges())
+        if (self.current_box_graph.edges() == self.init_graph.edges()):
+            return True
+        else:
+            return False
+
 
 class App:
     def __init__(self) -> None:
         self.gameData()
+        """
+        @rahil.jain
+        13/5/23
+        """
         self.initialise_root()
         self.title_section()
         self.connectivity_graph_section()
@@ -84,11 +169,11 @@ class App:
 
         title = tk.Canvas(self.connectivity_graph_frame, width=self.screen_width/4, height=self.screen_height/2)
         title.grid()
-        title.create_text(50, 50, text="Connectivity")
+        title.create_text(100, 50, text="Connectivity", font=helv8)
         f = plt.Figure(figsize=(3, 3), dpi=100)
         ax = f.add_subplot(111)
         g = nx.Graph()
-        g.add_edges_from(self.game_data.adjacency_graph)
+        g.add_edges_from(self.game_data.adjacency_list)
         nx.draw(g, ax=ax, with_labels=True, node_color="#ADD8E6")
         canvas = FigureCanvasTkAgg(f, self.connectivity_graph_frame)
 
@@ -102,7 +187,7 @@ class App:
         
         self.user_grid_frame = tk.Frame(self.root, width=self.screen_width*0.7, height=self.screen_height/2, highlightbackground="blue", highlightthickness=2)
         self.user_grid_frame.grid(row=1, column=1)
-        title = tk.Label(self.user_grid_frame, text="User Grid")
+        title = tk.Label(self.user_grid_frame, text="User Grid", font=helv8)
         title.place(x = 100, y = 50)
         self.create_bg_grid(self.game_data.grid_row_size, self.game_data.grid_col_size, self.user_grid_frame)
 
@@ -117,99 +202,116 @@ class App:
         for r in range(row):
             for c in range(col):
                 title = tk.Frame(self.user_grid_frame, highlightbackground="light grey", highlightthickness=1, width=50, height=50)
-                title.place(x = 100 + c*50 , y = 100 + 50*r)
+                title.place(x = 100 + c*self.game_data.width_quanta , y = 100 + self.game_data.height_quanta*r)
         pass
 
-    def drag_start(self, event, label):
-        label.startX = event.x
-        label.startY = event.y
+    def drag_start(self, event, i):
+        box = self.game_data.box_frames[i]
+        # saving the start point of the box
+        box.startX = event.x
+        box.startY = event.y
 
-    def drag_motion(self, event, label):
-        x = min(max(label.winfo_x() - label.startX + event.x, self.user_grid_frame.winfo_x()), self.user_grid_frame.winfo_x() + self.user_grid_frame.winfo_width() - label.winfo_width())
-        y = min(max(label.winfo_y() - label.startY + event.y, self.user_grid_frame.winfo_y()), self.user_grid_frame.winfo_y() + self.user_grid_frame.winfo_height() - label.winfo_height())
+    def drag_motion(self, event, i):
+        box = self.game_data.box_frames[i]
 
-        print(x)
-        print(y)
+        # finding the new location of the box
+        # x = min(max(box.winfo_x() - box.startX + event.x, self.user_grid_frame.winfo_x()), self.user_grid_frame.winfo_x() + self.user_grid_frame.winfo_width() - box.winfo_width())
+        # y = min(max(box.winfo_y() - box.startY + event.y, self.user_grid_frame.winfo_y()), self.user_grid_frame.winfo_y() + self.user_grid_frame.winfo_height() - box.winfo_height())
+
+        x = box.winfo_x() - box.startX + event.x
+        y = box.winfo_y() - box.startY + event.y
+        print("start")
+        print(x, y)
         user_grid_frame_x = self.user_grid_frame.winfo_x()
         user_grid_frame_y = self.user_grid_frame.winfo_y()
 
-        if(x > user_grid_frame_x + 100 and x < user_grid_frame_x + 100 + 50 * (self.game_data.grid_col_size)
-           and y > user_grid_frame_y + 100 and y < user_grid_frame_y + 100 + 50 * (self.game_data.grid_row_size)):
-            x = ((x - 100 - user_grid_frame_x) // 50) * 50 + 100 + user_grid_frame_x
-            y = ((y - 100 - user_grid_frame_y) // 50) * 50 + 100 + user_grid_frame_y
 
-        label.place(x = x, y = y)
+        # Snapping Functionality
+        # if(x > user_grid_frame_x + 100 and x < user_grid_frame_x + 100 + 50 * (self.game_data.grid_col_size)
+        #    and y > user_grid_frame_y + 100 and y < user_grid_frame_y + 100 + 50 * (self.game_data.grid_row_size)):
+        #     x = ((x - 100 - user_grid_frame_x) // self.game_data.width_quanta) * self.game_data.width_quanta + 100 + user_grid_frame_x
+        #     y = ((y - 100 - user_grid_frame_y) // self.game_data.height_quanta) * self.game_data.height_quanta + 100 + user_grid_frame_y
 
+        x = ((x - 122 - user_grid_frame_x) // self.game_data.width_quanta) * self.game_data.width_quanta + 122 + user_grid_frame_x
+        y = ((y - 135 - user_grid_frame_y) // self.game_data.height_quanta) * self.game_data.height_quanta + 135 + user_grid_frame_y
+        print(x, y)
+
+        print("start")
+
+        box.place(x = x, y = y)
+
+        self.game_data.boxes_cur_x[i]=x
+        self.game_data.boxes_cur_y[i]=y
+
+    def rotate_box(self, i):
+        print("rotating")
+        box = self.game_data.box_frames[i]
+
+        temp = self.game_data.box_sizes[i][0]
+        self.game_data.box_sizes[i][0] = self.game_data.box_sizes[i][1]
+        self.game_data.box_sizes[i][1] = temp
+
+        
+        print(box.winfo_width(), box.winfo_height())
+        print(self.game_data.box_sizes[i])
+
+        box.config(width =  box.winfo_height(), height=box.winfo_width())
     def inventory_section(self):
-        title = tk.Label(self.user_grid_frame, text="Boxes")
+        title = tk.Label(self.user_grid_frame, text="Boxes", font=helv8)
         title.place(x = 500, y = 50)
 
-        cur_x = 500
+        cur_x = 100
         cur_y = 100
 
         for i in range(len(self.game_data.box_sizes)):
-            title = tk.Frame(self.user_grid_frame, highlightthickness=2, width=50*self.game_data.box_sizes[i][0], height=50*self.game_data.box_sizes[i][1], bg=hex_colors[i])
-            title.place(x = cur_x, y = cur_y)
-            # label = tk.Label(title, text=i)
-            # label.grid()
+            print(i, "index")
+            self.game_data.box_frames[i] = tk.Frame(self.user_grid_frame, highlightthickness=2, width=self.game_data.width_quanta*self.game_data.box_sizes[i][0], height=self.game_data.height_quanta*self.game_data.box_sizes[i][1], bg=hex_colors[i])
+            self.game_data.box_frames[i].place(x = cur_x, y = cur_y)
+            box_num = tk.Label(self.game_data.box_frames[i], text=i, bg=hex_colors[i])
+            box_num.place(relx=0.5, rely=0.5, anchor="center")
+            self.game_data.box_frames[i].bind("<Button-1>", lambda event, i=i : self.drag_start(event, i))
+            self.game_data.box_frames[i].bind("<B1-Motion>", lambda event, i=i : self.drag_motion(event, i))
+            self.game_data.box_frames[i].bind("<Button-2>", lambda event, i=i : self.rotate_box(i))
+
+            box_num.bind("<Button-1>", lambda event, i=i : self.drag_start(event, i))
+            box_num.bind("<B1-Motion>", lambda event, i=i : self.drag_motion(event, i))
+            box_num.bind("<Button-2>", lambda event, i=i : self.rotate_box(i))
+            self.game_data.boxes_init_x[i]=cur_x
+            self.game_data.boxes_init_y[i]=cur_y
+            self.game_data.boxes_cur_x[i]=cur_x
+            self.game_data.boxes_cur_y[i]=cur_y
+
             cur_x += 150
-            if(cur_x > 1000):
+            if(cur_x > 1500):
                 cur_x = 500
                 cur_y += 150
-        pass
-        # self.inventory_frame = tk.Frame(self.root, width=self.screen_width/3, height=self.screen_height, highlightbackground="blue", highlightthickness=2)
-        # self.inventory_frame.grid(row=1, column=2)
-        # self.inventory_canvas = tk.Canvas(self.inventory_frame, width=self.screen_width/3, height=self.screen_height/2)
-        # self.inventory_canvas.grid()
-        # self.inventory_canvas.create_text(50, 50, text="Boxes")
 
-        self.boxes = {}
-        # rect = self.inventory_frame(50, 110,300,280, fill= "light blue")
-        label = tk.Frame(self.root, bg="red", width=100, height=100)
-        label.place(x= 900, y = 200)
-        label.bind("<Button-1>", lambda event : self.drag_start(event, label))
-        label.bind("<B1-Motion>", lambda event : self.drag_motion(event, label))
-        # self.boxes[rect] = {}
-        # self.inventory_canvas.tag_bind(rect, "<Button-1>", lambda event : self.drag_start(event, rect, self.inventory_canvas))
-        # self.inventory_canvas.tag_bind(rect, "<Button-1>", lambda x: print("hello"))
-        # self.inventory_canvas.tag_bind(rect, "<B1-Motion>", lambda event : self.drag_motion(event, rect, self.inventory_canvas))
-        # self.inventory_canvas.bind("<B1-Motion>", lambda event : self.drag_motion(event, rect))
-
-    # def drag_start(self, event, rect, canvas):
-    #     print(rect)
-    #     print(canvas.coords(rect))
-    #     self.boxes[rect]["startX"] = event.x
-    #     self.boxes[rect]["startY"] = event.y
-    #     # rect.startX = event.x
-    #     # rect.startY = event.y
-
-    # def drag_motion(self, event, rect, canvas):
-    #     # x = rect.winfo_x() - rect.startX + event.X
-    #     # y = rect.winfo_Y() - rect.startY + event.y
-    #     # rect.place(x=x, y=y)
-    #     pass
+        print(self.game_data.box_frames)
 
         
     def buttons_section(self):
         
-        self.connectivity_graph_frame = tk.Frame(self.root, width=self.screen_width/2, height=self.screen_height/10, highlightbackground="blue", highlightthickness=2)
-        self.connectivity_graph_frame.grid(row=2, column=0, columnspan=3)
+        self.buttons_section_frame = tk.Frame(self.root, width=self.screen_width/2, height=self.screen_height/10, highlightbackground="blue", highlightthickness=2)
+        self.buttons_section_frame.grid(row=2, column=0, columnspan=3)
 
-        self.create_btn = tk.Button(self.connectivity_graph_frame, text="Create new")
+        self.create_btn = tk.Button(self.buttons_section_frame, text="Create new")
+        self.output_text = tk.Label(self.buttons_section_frame,text="Keep Going....")
+        self.clear_btn = tk.Button(self.buttons_section_frame, text="Clear")
         self.create_btn.grid()
 
-        self.submit_btn = tk.Button(self.connectivity_graph_frame, text="Submit")
+        self.submit_btn = tk.Button(self.buttons_section_frame, text="Submit", command=lambda : self.game_data.check_game_success(self.output_text))
         self.submit_btn.grid()
 
-        self.clear_btn = tk.Button(self.connectivity_graph_frame, text="Clear")
         self.clear_btn.grid()
+
+        self.output_text.grid()
 
     def run(self):
         self.root.mainloop()
 
     def initialise_root(self):
         self.root = tk.Tk()
-        self.root.title("Rule Based GPLAN")
+        self.root.title("The Grid Game")
         self.screen_width = self.root.winfo_screenwidth()
         self.screen_height = self.root.winfo_screenheight()
         self.root.geometry(str(str(self.screen_width) +
@@ -220,16 +322,8 @@ class App:
         self.logo_frame.grid(row=0, column=0,columnspan=3)
         logo_canvas = tk.Canvas(self.logo_frame, width=self.screen_width*0.9, height=100)
         logo_canvas.pack()
-        logo_canvas.create_text(50, 50, text="GPLAN", font=helv15)
+        logo_canvas.create_text(200, 50, text="The Grid Game", font=helv15)
 
-
-    def checkered(canvas, line_distance):
-        # vertical lines at an interval of "line_distance" pixel
-        for x in range(line_distance,canvas_width,line_distance):
-            canvas.create_line(x, 0, x, canvas_height, fill="#476042")
-        # horizontal lines at an interval of "line_distance" pixel
-        for y in range(line_distance,canvas_height,line_distance):
-            canvas.create_line(0, y, canvas_width, y, fill="#476042")
 
 
 
